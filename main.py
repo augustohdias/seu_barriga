a03b3050125ee1c3d0a1d9fcf94df2bbc8b86480
Augusto Dias <augustohdiasz@gmail.com>
2022-06-24 13:21:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import bot.seu_barriga
 import concurrent.futures
 
 def main():
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+
+    class handler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            message = "Pong"
+            self.wfile.write(bytes(message, "utf8"))
+
     tasks = [bot.seu_barriga.notice_billing, bot.seu_barriga.read_updates]
     with concurrent.futures.ThreadPoolExecutor() as executor:
         running_tasks = [executor.submit(task) for task in tasks]
-        for running_task in running_tasks:
-            running_task.result()
     
+    with HTTPServer(('', 8000), handler) as server:
+        server.serve_forever()    
+
 if __name__ == "__main__":
     main()
