b2e29c16c41d3f9346ff8a001869934b4e779a86
Augusto Dias <augustohdiasz@gmail.com>
2022-06-25 14:13:53 -0300

    remove CRLF

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Como executar a aplicação:
-#   Copie o arquivo 'env' do diretorio config e renomeio para 'env.dev'
-#   Preencha com as variáveis de teste e execute este script
+# Como executar a aplicação localmente:
+#
+#   - Copie o arquivo 'config/env';
+#   - Renomeie a cópia para 'env.dev';
+#   - Preencha as variáveis de ambiente;
+#   - Execute este 'run.sh'
 
 source ./config/env.dev
 python3 ./src/main.py
