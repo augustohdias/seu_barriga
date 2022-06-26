f83e1af38f41a72af7cd098b0b2011d6d58dc63e
Augusto Dias <augustohdiasz@gmail.com>
2022-06-26 12:49:32 -0300

    muda parse mode

--- src/repository/telegram.py
+++ src/repository/telegram.py
@@ -11,16 +11,16 @@ class TelegramAPI(ChatServiceInterface):
         self.__bot_token = os.getenv('BOT_TOKEN', '')
     
     def send_message(self, message):
-        send_message_url = f'{self.__SERVER_URL}bot{self.__bot_token}/sendMessage?chat_id={self.__chat_id}&parse_mode=markdown&text={message}'
+        send_message_url = f'{self.__SERVER_URL}bot{self.__bot_token}/sendMessage?chat_id={self.__chat_id}&parse_mode=Markdown&text={message}'
         print(send_message_url)
         return requests.get(send_message_url).json()
     
     def send_private_message(self, message, user_id):
-        send_private_message_url = f'{self.__SERVER_URL}bot{self.__bot_token}/sendMessage?chat_id={user_id}&parse_mode=markdown&text={message}'
+        send_private_message_url = f'{self.__SERVER_URL}bot{self.__bot_token}/sendMessage?chat_id={user_id}&parse_mode=Markdown&text={message}'
         print(send_private_message_url)
         return requests.get(send_private_message_url).json()
 
     def reply_message(self, message, reply_to_id):
-        send_message_url = f'{self.__SERVER_URL}bot{self.__bot_token}/sendMessage?chat_id={self.__chat_id}&parse_mode=markdown&text={message}&reply_to_message_id={reply_to_id}'
+        send_message_url = f'{self.__SERVER_URL}bot{self.__bot_token}/sendMessage?chat_id={self.__chat_id}&parse_mode=Markdown&text={message}&reply_to_message_id={reply_to_id}'
         print(send_message_url)
         return requests.get(send_message_url).json()
